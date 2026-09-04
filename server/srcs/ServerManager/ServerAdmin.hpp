d37bc924f4d9450efac3219263dfded6be2c49aa
otmanoulcaid <ooulcaid.1337@gmail.com>
2026-09-04 22:14:56 +0100

    repo clean up

--- server/srcs/ServerManager/ServerAdmin.hpp
+++ server/srcs/ServerManager/ServerAdmin.hpp
@@ -12,16 +12,19 @@ class ServerAdmin : public Server
 		SSL_CTX 						*__ctx;
 		String							__certificate;
 		String							__privateKey;
+		String							__password;
 
 		ServerAdmin( const ServerAdmin &copy );
 		ServerAdmin	&operator=( const ServerAdmin &assign );
 
 		void							proccessCertificateToken( t_svec &tokens );
 		void							proccessPrivateKeyToken( t_svec &tokens );
+		void							proccessPasswordToken( t_svec &tokens );
 
 	public:
 		void							proccessToken( t_svec &tokens );
 		SSL_CTX							*getTLSContext();
+		bool							checkPassword( const String &password ) const;
 		void							setupSSL();
 
 		ServerAdmin( String line );
